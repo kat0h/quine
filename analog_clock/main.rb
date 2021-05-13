faa1b1ef702ecb426468a7b66d2b98435d297657
KotaKato <peony.btn@gmail.com>
2021-05-13 23:18:08 +0900

    [fix] 調節

--- analog_clock/main.rb
+++ analog_clock/main.rb
@@ -6,10 +6,10 @@ b=a.new(s){a.new(s,0)};
 #丸
 (0...s).each{|x|
   (0...s).each{|y|
-    xx=x-s/2;
-    yy=y-s/2;
+    xx=x-s/2.1;
+    yy=y-s/2.1;
     i=xx**2+yy**2;
-    b[y][x]=(s/3)**2<i&&i<(s/2)**2?1:0;
+    b[y][x]=(s/2.5)**2<i&&i<(s/2)**2?1:0;
   }
 }
 
@@ -18,7 +18,8 @@ l=Math;
 t=Time.new;
 h,m=t.hour%12*0.5,t.min*0.1;
 #長針・短針
-b[(-l.cos(m)*(s/3.5)+s/2).to_i][(l.sin(m)*(s/3.5)+s/2).to_i]=?%;
-b[(-l.cos(h)*(s/4.5)+s/2).to_i][(l.sin(h)*(s/4.5)+s/2).to_i]=?%;
+b[(-l.cos(m)*(s/3)+s/2).to_i][(l.sin(m)*(s/3)+s/2).to_i]=?%;
+b[(-l.cos(h)*(s/4)+s/2).to_i][(l.sin(h)*(s/4)+s/2).to_i]=?%;
+
 
 b.each{|y|y.each{|x|print(x==0??\s*2:x.to_s*2)};puts};
